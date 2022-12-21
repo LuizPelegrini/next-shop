852f902113edc48937f54e06311e94dc843937f7
Luiz Guilherme <ldesouzapelegrini01@gmail.com>
2022-12-21 23:24:01 +0800

    add: Integrate with Stitches library

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,11 @@
+import { styled } from '../styles';
+
+const Button = styled('button', {
+  backgroundColor: '$primary',
+});
+
 export default function Home() {
   return (
-    <h1>Hello World</h1>
+    <Button>Hello World</Button>
   );
 }
-    
