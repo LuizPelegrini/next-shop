74c3bac3a1883ed5ff868026aeeaed87dffc8f30
Luiz Guilherme <ldesouzapelegrini01@gmail.com>
2022-12-22 15:11:57 +0800

    add: Style static product cards

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,35 @@
-import { styled } from '../styles';
+import Image from 'next/image';
 
-const Button = styled('button', {
-  backgroundColor: '$primary',
-});
+import { Container, Product } from '../styles/pages/home';
+
+import tshirt1 from '../assets/tshirts/1.png';
+import tshirt2 from '../assets/tshirts/2.png';
+import tshirt3 from '../assets/tshirts/3.png';
 
 export default function Home() {
   return (
-    <Button>Hello World</Button>
+    <Container>
+      <Product>
+        <Image src={tshirt1} width={520} height={480} alt=""/>
+        <footer>
+          <strong>T-Shirt X</strong>
+          <span>$19.90</span>
+        </footer>
+      </Product>
+      <Product>
+        <Image src={tshirt2} width={520} height={520} alt=""/>
+        <footer>
+          <strong>T-Shirt X</strong>
+          <span>$19.90</span>
+        </footer>
+      </Product>
+      <Product>
+        <Image src={tshirt3} width={520} height={520} alt=""/>
+        <footer>
+          <strong>T-Shirt X</strong>
+          <span>$19.90</span>
+        </footer>
+      </Product>
+    </Container>
   );
 }
