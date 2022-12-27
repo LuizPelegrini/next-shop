8f23ece2cf96e9cc6f1a03aefe5d56294048522a
Luiz Guilherme <ldesouzapelegrini01@gmail.com>
2022-12-27 15:31:38 +0800

    add: Scaffold product page

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,9 +1,23 @@
 import { useRouter } from "next/router";
+import { Container, ImageContainer, ProductDetails } from "../../styles/pages/product";
 
 export default function Product() {
   const { query } = useRouter();
 
   return (
-    <h1>Product: { JSON.stringify(query)}</h1>
+    <Container>
+      <ImageContainer>
+
+      </ImageContainer>
+
+      <ProductDetails>
+        <h1>Tshirt X</h1>
+        <span>$19.90</span>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste beatae, laudantium, deleniti obcaecati sed pariatur quibusdam mollitia repellat consectetur tenetur minima necessitatibus dolore ipsa dolor dolorum eveniet temporibus veniam neque?</p>
+
+        <button>Buy</button>
+      </ProductDetails>
+    </Container>
   );
 }
